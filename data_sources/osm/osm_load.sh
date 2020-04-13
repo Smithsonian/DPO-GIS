49c50e5f100f4ebf1f1d435753aa686cee21ebe2
Luis J. Villanueva <villanueval@si.edu>
2020-04-13 10:41:06 -0400

    Update osm_load.sh

--- data_sources/osm/osm_load.sh
+++ data_sources/osm/osm_load.sh
@@ -15,12 +15,12 @@ cols=(amenity barrier bridge building embankment harbour highway historic landus
 
 
 #Download each file and load it to psql using osm2pgsql
-for i in *.pbf; do
+for j in *.pbf; do
     echo ""
-    echo "Working on file $i..."
+    echo "Working on file $j..."
     echo ""
     #Import pbf to postgres
-    osm2pgsql --latlong --username gisuser --host localhost --database osm -C 16000 --create --slim --number-processes 8 --multi-geometry --verbose --flat-nodes /mnt/fastdisk/tmp/mycache.bin $i >> osm_$i.log
+    osm2pgsql --latlong --username gisuser --host localhost --database osm -C 16000 --create --slim --number-processes 8 --multi-geometry --verbose --flat-nodes /mnt/fastdisk/tmp/mycache.bin $j
 
 
     #Execute for each column
@@ -49,7 +49,7 @@ for i in *.pbf; do
                                                 st_centroid(st_multi(way)),
                                                 st_multi(way) as the_geom,
                                                 st_transform(st_multi(way), 3857) as the_geom_webmercator,
-                                                '$i'
+                                                '$j'
                                             from 
                                                 data d LEFT JOIN 
                                                     planet_osm_ways r ON 
