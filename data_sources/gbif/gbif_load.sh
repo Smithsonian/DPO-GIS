07aeb635065f184bcf13c27ad2d5329bf0d7f220
Luis J. Villanueva <villanueval@si.edu>
2020-04-13 10:33:29 -0400

    updating source scripts

--- data_sources/gbif/gbif_load.sh
+++ data_sources/gbif/gbif_load.sh
@@ -6,7 +6,7 @@
 #    https://gbif.org
 # 
 # Then, unzip the files that the script will use
-#    unzip 000##########.zip -x meta.xml verbatim.txt citations.txt multimedia.txt rights.txt
+#    unzip 000##########.zip -x occurrence.txt meta.xml verbatim.txt citations.txt multimedia.txt rights.txt
 # 
 
 #Today's date
@@ -20,12 +20,8 @@ script_date=$(date +'%Y-%m-%d')
 # rm multimedia.txt
 # rm rights.txt
 
-#Replace backslashes in some text fields
-sed -i 's.\\./.g' occurrence.txt
-
-#Break into smaller pieces, each with 5M rows
-split -l 5000000 occurrence.txt gbifdwc
-rm occurrence.txt
+#unzip file and break into pieces via pipes
+unzip -p 000.zip occurrence.txt | split -l 5 - gbifdwc
 
 #Remove first line (header) in the first file
 sed -i '1d' gbifdwcaa
@@ -39,6 +35,8 @@ psql -U gisuser -h localhost gis < gbif_tables.sql
 # Load the segments to the occurrence table, then import to the final, and simplified, gbif table
 for file in gbifdwc*; do
     echo $file
+    #Replace backslashes in some text fields
+    sed -i 's.\\./.g' $file
     psql -U gisuser -h localhost gis -c "\copy gbif_occ FROM '$file';"
     psql -U gisuser -h localhost gis -c "INSERT INTO gbif (gbifID, eventDate, basisOfRecord, recordedBy, occurrenceID, locationID, continent, waterBody, islandGroup, island, countryCode, stateProvince, county, municipality, locality, verbatimLocality, locationAccordingTo, locationRemarks, decimalLatitude, decimalLongitude, coordinateUncertaintyInMeters, coordinatePrecision, pointRadiusSpatialFit, georeferencedBy, georeferencedDate, georeferenceProtocol, georeferenceSources, georeferenceVerificationStatus, georeferenceRemarks, taxonConceptID, scientificName, higherClassification, kingdom, phylum, class, _order, family, genus, subgenus, specificEpithet, infraspecificEpithet, taxonRank, vernacularName, nomenclaturalCode, taxonomicStatus, nomenclaturalStatus, taxonRemarks, datasetKey, issue, hasGeospatialIssues, taxonKey, acceptedTaxonKey, species, genericName, acceptedScientificName, the_geom, the_geom_webmercator) (SELECT gbifID, eventDate, basisOfRecord, recordedBy, occurrenceID, locationID, continent, waterBody, islandGroup, island, countryCode, stateProvince, county, municipality, locality, verbatimLocality, locationAccordingTo, locationRemarks, decimalLatitude, decimalLongitude, coordinateUncertaintyInMeters, coordinatePrecision, pointRadiusSpatialFit, georeferencedBy, georeferencedDate, georeferenceProtocol, georeferenceSources, georeferenceVerificationStatus, georeferenceRemarks, taxonConceptID, scientificName, higherClassification, kingdom, phylum, class, _order, family, genus, subgenus, specificEpithet, infraspecificEpithet, taxonRank, vernacularName, nomenclaturalCode, taxonomicStatus, nomenclaturalStatus, taxonRemarks, datasetKey, issue, hasGeospatialIssues, taxonKey, acceptedTaxonKey, species, genericName, acceptedScientificName, ST_SETSRID(ST_POINT(decimalLongitude, decimalLatitude), 4326) as the_geom, ST_TRANSFORM(ST_SETSRID(ST_POINT(decimalLongitude, decimalLatitude), 4326), 3857) as the_geom_webmercator FROM gbif_occ WHERE locality != '' AND species != '' AND decimalLongitude != 0 AND decimalLatitude != 0 AND decimalLongitude IS NOT NULL AND decimalLatitude IS NOT NULL AND decimalLongitude != 180 AND decimalLatitude != 90 AND decimalLongitude != -180 AND decimalLatitude != -90);"
     psql -U gisuser -h localhost gis -c "TRUNCATE gbif_occ;"
@@ -59,9 +57,9 @@ rm gbifdatasets.csv
 rm -r dataset
 
 #Create indices, other post-insert cleanup
-psql -U gisuser -h localhost gis < post_insert_indices2.sql &
-psql -U gisuser -h localhost gis < post_insert_indices3.sql &
-psql -U gisuser -h localhost gis < post_insert_indices4.sql &
+psql -U gisuser -h localhost gis < post_insert_indices2.sql
+psql -U gisuser -h localhost gis < post_insert_indices3.sql
+psql -U gisuser -h localhost gis < post_insert_indices4.sql
 psql -U gisuser -h localhost gis < post_insert_indices.sql 
 
 #GBIF plant specimens
@@ -73,4 +71,4 @@ title_doi=`xpath -q -e '//dataset/title/text()' metadata.xml`
 #rm metadata.xml
 
 #Turn datasource online
-psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 't', source_date = '$script_date', source_title = '$title_doi' WHERE datasource_id = 'gbif';"
+psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 't', source_date = '$script_date', source_title = '$title_doi', no_features = w.no_feats FROM (select count(*) as no_feats from gbif) w WHERE datasource_id = 'gbif';"
--- data_sources/osm/osm_load.sh
+++ data_sources/osm/osm_load.sh
@@ -3,50 +3,24 @@
 #Get the OSM extracts from geofabrik.de and refresh the PostGIS database
 # using osm2pgsql (https://wiki.openstreetmap.org/wiki/Osm2pgsql)
 # 
+# This version uses the country-level files to reduce the resources
+#  needed for oms2pgsql.
+#
 
-
-#Today's date
-script_date=$(date +'%Y-%m-%d')
-
-
-#Delete unused tables
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_line CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_nodes CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_point CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_rels CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_roads CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_polygon CASCADE;"
-
-#Turn datasource offline
-psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 'f' WHERE datasource_id = 'osm';"
-
-#Drop indices before bulk loading
-psql -U gisuser -h localhost osm -c "DROP INDEX IF EXISTS osm_name_idx;"
-psql -U gisuser -h localhost osm -c "DROP INDEX IF EXISTS osm_thegeom_idx;"
-psql -U gisuser -h localhost osm -c "DROP INDEX IF EXISTS osm_thegeomw_idx;"
-psql -U gisuser -h localhost osm -c "DROP INDEX IF EXISTS osm_centroid_idx;"
-
-#Empty table
-psql -U gisuser -h localhost osm -c "TRUNCATE osm;"
-psql -U gisuser -h localhost osm -c "VACUUM osm;"
 
 
 #Columns to get the type
 cols=(amenity barrier bridge building embankment harbour highway historic landuse leisure lock man_made military motorcar natural office place public_transport railway religion service shop sport surface toll tourism tunnel water waterway wetland wood)
 
-#List of files that geofabrik makes available
-osm_files=(north-america-latest.osm.pbf antarctica-latest.osm.pbf central-america-latest.osm.pbf south-america-latest.osm.pbf africa-latest.osm.pbf asia-latest.osm.pbf australia-oceania-latest.osm.pbf europe-latest.osm.pbf)
+
 
 #Download each file and load it to psql using osm2pgsql
-for i in ${!osm_files[@]}; do
+for i in *.pbf; do
     echo ""
-    echo "Working on file ${osm_files[$i]}..."
+    echo "Working on file $i..."
     echo ""
-    #Download using wget
-    wget -c -a osm.log https://download.geofabrik.de/${osm_files[$i]}
-    #Append to existing tables
-    osm2pgsql --latlong --username gisuser --host localhost --database osm -C 22000 --create --number-processes 8 --multi-geometry --verbose --flat-nodes /mnt/fastdisk/tmp/mycache.bin ${osm_files[$i]} >> osm.log
-    #rm ${osm_files[$i]}
+    #Import pbf to postgres
+    osm2pgsql --latlong --username gisuser --host localhost --database osm -C 16000 --create --slim --number-processes 8 --multi-geometry --verbose --flat-nodes /mnt/fastdisk/tmp/mycache.bin $i >> osm_$i.log
 
 
     #Execute for each column
@@ -62,10 +36,11 @@ for i in ${!osm_files[@]}; do
                                                 from 
                                                     planet_osm_polygon 
                                                 where 
-                                                    \"${cols[$i]}\" IS NOT NULL
+                                                    \"${cols[$i]}\" IS NOT NULL AND
+                                                    name IS NOT NULL
                                             )
                                         INSERT INTO osm 
-                                            (source_id, name, type, attributes, centroid, the_geom, the_geom_webmercator) 
+                                            (source_id, name, type, attributes, centroid, the_geom, the_geom_webmercator, data_source) 
                                             select 
                                                 d.osm_id::text, 
                                                 d.name, 
@@ -73,37 +48,15 @@ for i in ${!osm_files[@]}; do
                                                 tags::hstore,
                                                 st_centroid(st_multi(way)),
                                                 st_multi(way) as the_geom,
-                                                st_transform(st_multi(way), 3857) as the_geom_webmercator
+                                                st_transform(st_multi(way), 3857) as the_geom_webmercator,
+                                                '$i'
                                             from 
                                                 data d LEFT JOIN 
                                                     planet_osm_ways r ON 
                                                     (d.osm_id = r.id);"
+        psql -U gisuser -h localhost osm -c "DROP INDEX osmplanet_${cols[$i]}_idx;"
         done
-        #psql -U gisuser -h localhost osm -c "DROP INDEX osmplanet_${cols[$i]}_idx;"
     done
-done
 
 
-
-
-#Recreate indices
-psql -U gisuser -h localhost osm -c "CREATE INDEX osm_name_idx ON osm USING BTREE(name);"
-psql -U gisuser -h localhost osm -c "CREATE INDEX osm_thegeom_idx ON osm USING GIST(the_geom);"
-psql -U gisuser -h localhost osm -c "CREATE INDEX osm_thegeomw_idx ON osm USING GIST(the_geom_webmercator);"
-psql -U gisuser -h localhost osm -c "CREATE INDEX osm_centroid_idx ON osm USING GIST(centroid);"
-
-#Move table between dbs
-#####
-#####
-
-#Turn datasource online
-psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 't', source_date = '$script_date' WHERE datasource_id = 'osm';"
-
-#Delete last tables
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_line CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_nodes CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_point CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_rels CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_roads CASCADE;"
-psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_polygon CASCADE;"
 
