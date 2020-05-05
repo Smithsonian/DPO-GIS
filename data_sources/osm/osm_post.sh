26eada0e26f22a36d548d1f5d651534e35c3fa81
Luis J. Villanueva <villanueval@si.edu>
2020-05-05 11:43:49 -0400

    Update osm_post.sh

--- data_sources/osm/osm_post.sh
+++ data_sources/osm/osm_post.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
 #
-#Get the OSM extracts from geofabrik.de and refresh the PostGIS database
-# using osm2pgsql (https://wiki.openstreetmap.org/wiki/Osm2pgsql)
-# 
-
 
 #Today's date
 script_date=$(date +'%Y-%m-%d')
 
+# #Delete last tables
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_line CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_nodes CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_point CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_rels CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_roads CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_polygon CASCADE;"
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_ways CASCADE;"
 
 
-#Recreate indices
-# psql -U gisuser -h localhost osm -c "CREATE INDEX osm_name_idx ON osm USING BTREE(name);"
-# psql -U gisuser -h localhost osm -c "CREATE INDEX osm_thegeom_idx ON osm USING GIST(the_geom);"
-# psql -U gisuser -h localhost osm -c "CREATE INDEX osm_thegeomw_idx ON osm USING GIST(the_geom_webmercator);"
-# psql -U gisuser -h localhost osm -c "CREATE INDEX osm_centroid_idx ON osm USING GIST(centroid);"
+#Move to main database
+pg_dump -U gisuser -h localhost -t osm osm | psql -U gisuser -h localhost gis 
 
-#Move table between dbs
-#####
-#####
+#Indices
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_uid_idx ON osm USING BTREE(uid);"
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_name_idx ON osm USING gin (name gin_trgm_ops);"
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_thegeom_idx ON osm USING GIST(the_geom);"
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_thegeomw_idx ON osm USING GIST(the_geom_webmercator);"
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_centroid_idx ON osm USING GIST(centroid);"
+
 
 #Turn datasource online
-# psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 't', source_date = '$script_date' WHERE datasource_id = 'osm';"
+psql -U gisuser -h localhost gis -c "UPDATE data_sources SET is_online = 't', source_date = '$script_date', no_features = w.no_feats FROM (select count(*) as no_feats from osm) w WHERE datasource_id = 'osm';"
 
-# #Delete last tables
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_line CASCADE;"
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_nodes CASCADE;"
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_point CASCADE;"
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_rels CASCADE;"
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_roads CASCADE;"
-# psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS planet_osm_polygon CASCADE;"
+#Drop from osm
+psql -U gisuser -h localhost osm -c "DROP TABLE IF EXISTS osm CASCADE;"
+
+psql -U gisuser -h localhost gis -c "ALTER TABLE osm ADD COLUMN gadm2 text;"
+
+psql -U gisuser -h localhost gis -c "ALTER TABLE osm ADD COLUMN country text;"
+
+psql -U gisuser -h localhost gis -c "WITH data AS (
+    SELECT 
+        w.uid,
+        string_agg(g.name_2 || ', ' || g.name_1 || ', ' || g.name_0, '; ') as loc,
+        max(g.name_0) as name_0
+    FROM 
+        osm w,
+        gadm2 g
+    WHERE 
+        ST_INTERSECTS(w.the_geom, g.the_geom)
+    GROUP BY 
+        w.uid
+)
+UPDATE osm g SET gadm2 = d.loc, country = d.name_0 FROM data d WHERE g.uid = d.uid;"
+
+
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_gadm2_idx ON osm USING gin (gadm2 gin_trgm_ops);"
+
+psql -U gisuser -h localhost gis -c "CREATE INDEX osm_country_idx ON osm USING BTREE(country);"
